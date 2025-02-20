7e4090ddf27e528271b43478080699d2c6d188eb
Jason_Jie <1317674293@qq.com>
2025-02-20 22:05:06 +0800

    Add start running attack script

---
+++ scripts/running_attack.sh
@@ -0,0 +1,5 @@
+
+gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
+--tab -e 'bash -c "sleep 5; roslaunch px4 attack.launch; exec bash"' \
+--tab -e 'bash -c "sleep 6; rosrun joy joy_node; exec bash"' \
+--tab -e 'bash -c "sleep 7; rosrun proportional_navigation_node proportional_navigation_node; exec bash"' \
