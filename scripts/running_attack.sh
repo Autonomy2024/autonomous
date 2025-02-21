47e04a3c0004a3474c81d81dc0ae783530b1b8e5
Jason_Jie <1317674293@qq.com>
2025-02-21 14:13:41 +0800

    chore: Add auto start lighttrack node in scripts

--- scripts/running_attack.sh
+++ scripts/running_attack.sh
@@ -3,3 +3,5 @@ gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
 --tab -e 'bash -c "sleep 5; roslaunch px4 attack.launch; exec bash"' \
 --tab -e 'bash -c "sleep 6; rosrun joy joy_node; exec bash"' \
 --tab -e 'bash -c "sleep 7; rosrun proportional_navigation_node proportional_navigation_node; exec bash"' \
+--tab -e 'bash -c "sleep 10; rosrun lighttrack_ros LightTrackNode; exec bash"' \
+
